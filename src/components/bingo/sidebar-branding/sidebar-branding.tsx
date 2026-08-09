4150f9d7ee0411f883eb22feabf0af24b6298227
GusGrecco <145725375+GusGrecco@users.noreply.github.com>
2026-08-09 16:21:27 -0300

    feat: add sidebar branding component and logo

---
+++ src/components/bingo/sidebar-branding/sidebar-branding.tsx
@@ -0,0 +1,10 @@
+import binguLogo from "@/assets/bingu-logo-v2.png";
+
+export const SidebarBranding = () => {
+
+    return (
+        <div>
+            <img src={binguLogo} alt="Branding Logo" />
+        </div>
+    )
+}
